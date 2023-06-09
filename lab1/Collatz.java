bf58b7b60eec3d8b509156c2ac72e145420c34f8
unknown <zhangjiaqiang@chinamobile.com>
2023-06-09 17:15:06 +0800

    Add lab1.D Collatz implementation

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,13 +4,21 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
+    public static int buggyNextNumber(int n) {
         if (n  == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
             return n * 2;
+        }
+    }
+    /** current implementation of Collatz seq: see https://en.wikipedia.org/wiki/Collatz_conjecture */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
         }
     }
 
