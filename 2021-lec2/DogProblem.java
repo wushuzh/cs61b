8325f7604a087f385f6b3541d4f3c45051fa1cf0
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-12 15:04:36 +0800

    Add 3 helper methods and 2 test cases

--- 2021-lec2/DogProblem.java
+++ 2021-lec2/DogProblem.java
@@ -1,11 +1,60 @@
 public class DogProblem {
 
     public static Dog[] largerThanFourNeighbours(Dog[] dogs) {
-        return dogs;
+        /** we don't know how many dogs will be, so we have to initialized with max capacity with array
+         * The problem is when the array is not filled with every entry, there will be null
+         * The fix would be keeping a count of largerDog we found during the process
+         * */
+
+        Dog[] resultDogs = new Dog[dogs.length];
+        int count = 0;
+
+        for(int i = 0; i < dogs.length; i++) {
+            if (isLargerDog(dogs, i)) {
+                resultDogs[count] = dogs[i];
+                count++;
+            }
+        }
+
+        Dog[] noNullDogs = removeNullFromArrayDogs(resultDogs, count);
+
+        return noNullDogs;
+    }
+
+    private static boolean isLargerDog(Dog[] dogs, int i) {
+        boolean result = true;
+        for(int j = -2; j <= 2; j++) {
+
+            int compareIdx = i + j;
+
+            if (isValidIdx(dogs, compareIdx)
+                    && dogs[compareIdx].weightInPounds > dogs[i].weightInPounds) {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    private static boolean isValidIdx(Dog[] dogs, int compareIdx) {
+        boolean result = false;
+
+        if (compareIdx >= 0 && compareIdx < dogs.length) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private static Dog[] removeNullFromArrayDogs(Dog[] dogs, int count) {
+        Dog[] result = new Dog[count];
+        for(int i = 0; i < count; i++) {
+                result[i] = dogs[i];
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        Dog[] dogs = new Dog[] {
+        Dog[] dogs1 = new Dog[] {
                 new Dog(10),
                 new Dog(20),
                 new Dog(30),
@@ -15,9 +64,27 @@ public class DogProblem {
                 new Dog(10)
         };
 
-        Dog[] largerDogs = largerThanFourNeighbours(dogs);
-        for(int i = 0; i < largerDogs.length; i++) {
-            System.out.print(largerDogs[i].weightInPounds + " ");
+        Dog[] largerDogs1 = largerThanFourNeighbours(dogs1);
+        for(int i = 0; i < largerDogs1.length; i++) {
+            System.out.print(largerDogs1[i].weightInPounds + " ");
         }
+        System.out.println("expect: [30, 40]");
+
+        Dog[] dogs2 = new Dog[] {
+                new Dog(10),
+                new Dog(15),
+                new Dog(20),
+                new Dog(10),
+                new Dog(5),
+                new Dog(10),
+                new Dog(15),
+                new Dog(22),
+                new Dog(20)
+        };
+        Dog[] largerDogs2 = largerThanFourNeighbours(dogs2);
+        for(int i=0; i< largerDogs2.length; i++) {
+            System.out.print(largerDogs2[i].weightInPounds + " ");
+        }
+        System.out.println(" expect: [20, 22]");
     }
 }
