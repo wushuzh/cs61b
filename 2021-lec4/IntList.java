9563fe412ef9efb2d968bb0cdc5d610569321846
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-13 12:06:56 +0800

    Add recursive impl of incrList and dincrList

--- 2021-lec4/IntList.java
+++ 2021-lec4/IntList.java
@@ -40,6 +40,29 @@ public class IntList {
         return p.first;
     }
 
+    /** Returns an IntList identical to L, but with all values incremented by x.
+     *  Valuse in L cannot change !
+     *  */
+    public static IntList incrList(IntList L, int x) {
+        if (L != null) {
+            return new IntList(L.first + x, incrList(L.rest, x));
+        }
+        return null;
+    }
+
+    /** Returns an IntList identical to L, but with all values incremented by x.
+     * Not allowed to use 'new' (to save memory
+     * */
+    public static IntList dincrList(IntList L, int x) {
+        if (L == null) {
+            return null;
+        }
+        L.first += x;
+        L.rest = dincrList(L.rest, x);
+
+        return L;
+    }
+
     public static void main(String[] args) {
         IntList L1 = new IntList(15, null);
         L1 = new IntList(10, L1);
@@ -48,9 +71,21 @@ public class IntList {
         System.out.println(L1.size());
         System.out.println(L1.iterativeSize());
 
+        System.out.println("IncrList by 5:");
+        IntList L1plus5 = incrList(L1, 5);
+        System.out.println(L1plus5.get(0));
+        System.out.println(L1plus5.get(1));
+        System.out.println(L1plus5.get(2));
+
         System.out.println(L1.get(0));
         System.out.println(L1.get(1));
         System.out.println(L1.get(2));
+
+        System.out.println("dIncrList by 5:");
+        IntList L2plus5 = dincrList(L1, 5);
+        System.out.println(L2plus5.get(0));
+        System.out.println(L2plus5.get(1));
+        System.out.println(L2plus5.get(2));
 
         System.out.println(L1.iterativeGet(0));
         System.out.println(L1.iterativeGet(1));
