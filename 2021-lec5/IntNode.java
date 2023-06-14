3c30072deb7982c48b8aa6fc3593973a19546445
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-14 10:00:30 +0800

    Add SLList with IntNode initial code

---
+++ 2021-lec5/IntNode.java
@@ -0,0 +1,9 @@
+public class IntNode {
+    int item;
+    IntNode next;
+
+    public IntNode(int i, IntNode n) {
+        item = i;
+        next = n;
+    }
+}
---
+++ 2021-lec5/SLList.java
@@ -0,0 +1,27 @@
+public class SLList {
+    public IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    public int getFirst() {
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        //IntList oldL = new IntList(10, null);
+        //System.out.println(oldL.first);
+
+        SLList newL = new SLList(10);
+        System.out.println(newL.first.item);
+        System.out.println(newL.getFirst());
+
+        newL.addFirst(9);
+        System.out.println(newL.getFirst());
+    }
+}
