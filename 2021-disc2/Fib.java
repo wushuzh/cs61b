b7e942c378ba8f8295878cc43be34bb3bfe39fac
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-19 09:27:24 +0800

    Impl fib2 by asking user to provides init 2 nums

--- 2021-disc2/Fib.java
+++ 2021-disc2/Fib.java
@@ -4,12 +4,26 @@ public class Fib {
         if (n == 2) return 1;
         return fib(n - 1) + fib(n-2);
     }
+    
+    public static int fib2(int n, int k, int f0, int f1) {
+        if (n - 1 == k) {
+            return f0;
+        } else {
+            return fib2(n, k+1, f1, f0 + f1);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("The first 9 numbers should be 0 1 1 2 3 5 8 13 21 ");
-        System.out.print("My programming produces this: ");
+        System.out.print("My programmer fib1 produces : ");
         for(int i = 1; i < 10; i++) {
             System.out.print(fib(i) + " ");
+        }
+
+        System.out.println();
+        System.out.print("My programmer fib2 produces : ");
+        for(int i = 1; i < 10; i++) {
+            System.out.print(fib2(i, 0, 0, 1) + " ");
         }
     }
 }
