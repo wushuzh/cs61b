4a3cf356c60d679b5725ab4a980ddf7c8ffab310
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-25 17:37:56 +0800

    Add test with a fixed size list with buggy list

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,22 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> fixedList = new AListNoResizing<>();
+        BuggyAList<Integer> buggyList = new BuggyAList<>();
+
+        for (int i: new int[]{4, 5, 6}) {
+            fixedList.addLast(i);
+            buggyList.addLast(i);
+        }
+
+        for (int i = 3; i < 0; i++) {
+            assertEquals(i, buggyList.size());
+            assertEquals(fixedList.size(), buggyList.size());
+            assertEquals(fixedList.removeLast(), buggyList.removeLast());
+        }
+
+    }
 }
