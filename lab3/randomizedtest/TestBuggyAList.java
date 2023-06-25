fc265511d09bf63f8bf652f8e33ecb8d0a592d03
wushuzh <zhangjiaqiang@chinamobile.com>
2023-06-25 18:12:52 +0800

    Add randomized test and Fix bug

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -26,4 +26,36 @@ public class TestBuggyAList {
         }
 
     }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> buggyL = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i++) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                buggyL.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int sizeL = L.size();
+                int sizeBL = buggyL.size();
+                assertEquals("The size check :", sizeL, sizeBL);
+            } else if (operationNumber == 2 && L.size() > 0) {
+                // getLast
+                int returnL = L.getLast();
+                int returnBL = buggyL.getLast();
+                assertEquals("The getLast check : ", returnL, returnBL);
+            } else if (operationNumber == 3 && L.size() > 0) {
+                // removeLast
+                int removeL = L.removeLast();
+                int removeBL = buggyL.removeLast();
+                assertEquals("The removeLast check: ", removeL, removeBL);
+            }
+        }
+    }
 }
